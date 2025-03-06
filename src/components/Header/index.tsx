f77fd2de2273db37cf633eaf18b17fb7fe90db72
victorbraga8 <contato@victorbraga.com.br>
2025-03-05 23:46:39 -0300

    chore: commit for testing

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,16 +1,16 @@
-import styles from './styles.module.scss';
-import format from 'date-fns/format';
-import ptBR from 'date-fns/locale/pt-BR';
+import styles from "./styles.module.scss";
+import format from "date-fns/format";
+import ptBR from "date-fns/locale/pt-BR";
 
-export function Header(){
-    const date = format(new Date(), 'EEEEEE, d MMMM', {
-        locale: ptBR
-    });
-        return (
-        <header className={styles.headerContainer}>
-            <img src="/logo.svg" alt="PodCastr"/>
-            <p>O melhor para você ouvir, sempre</p>
-            <span>{date}</span>
-        </header>
-    );
+export function Header() {
+  const date = format(new Date(), "EEEEEE, d MMMM", {
+    locale: ptBR,
+  });
+  return (
+    <header className={styles.headerContainer}>
+      <img src="/logo.svg" alt="PodCastr" />
+      <p>O melhor para você ouvir, sempr e</p>
+      <span>{date}</span>
+    </header>
+  );
 }
